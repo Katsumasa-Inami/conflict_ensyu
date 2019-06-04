cb160d393ad05a629404cc06a2a0d597b55a6b05
yuto takaiwa <y.takaiwa1811@morijyobi.ac.jp>
2019-06-04 11:18:23 +0900

    [modify]Conflict.java更新

--- Conflict.java
+++ Conflict.java
@@ -1,6 +1,9 @@
 public class Conflict(){
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		for(int i=0;i<10;i++){
+			int r=java.util.Random().nextInt(101)+1;
+			System.out.println(r);
+		}
 	}
 }
 
